f74aa720d0c6f34913d99834a6ea52429aa0ae85
Kiran <f20202521@hyderabad.bits-pilani.ac.in>
2024-04-28 17:20:36 +0530

    added comments

--- rna.c
+++ rna.c
@@ -5,10 +5,12 @@
 #include <time.h>
 #include <math.h>
 
-bool isValidPairing(char base1, char base2) {
+// Check if two bases form a valid base pair
+bool isValidBasePair(char base1, char base2) {
     return (base1 == 'A' && base2 == 'U') || (base1 == 'U' && base2 == 'A') || (base1 == 'C' && base2 == 'G') || (base1 == 'G' && base2 == 'C');
 }
 
+// Check if the indices form a kink (base pairs too close)
 bool isKink(int idx1, int idx2) {
     return (idx1 >= idx2 - 4);
 }
@@ -21,20 +23,21 @@ typedef struct {
 typedef struct {
     char* sequence;
     int len;
-    int ans;
+    int maxPairs;
     int** maxMatchings;
     Match* matches;
     int curMatches;
-    double exec_time;
+    double execTime;
 } Molecule;
 
-void calculateMaxMatchings(Molecule* mol) {
+// Calculate the maximum number of base pairs using dynamic programming
+void calculateMaxBasePairs(Molecule* mol) {
     for (int sz = 6; sz <= mol->len; sz++) {
         for (int i = 0; i + sz - 1 < mol->len; i++) {
             int j = i + sz - 1;
             mol->maxMatchings[i][j] = mol->maxMatchings[i][j - 1];
             for (int t = i; t <= j; t++) {
-                if (isValidPairing(mol->sequence[t], mol->sequence[j]) && !isKink(t, j)) {
+                if (isValidBasePair(mol->sequence[t], mol->sequence[j]) && !isKink(t, j)) {
                     if (t == i)
                         mol->maxMatchings[i][j] = fmax(mol->maxMatchings[i][j], 1 + mol->maxMatchings[t + 1][j - 1]);
                     else
@@ -43,43 +46,46 @@ void calculateMaxMatchings(Molecule* mol) {
             }
         }
     }
-    mol->ans = mol->maxMatchings[0][mol->len - 1];
+    mol->maxPairs = mol->maxMatchings[0][mol->len - 1];
 }
 
-void calculateMatchings(Molecule* mol, int l, int r) {
-    if (mol->curMatches == mol->ans || mol->maxMatchings[l][r] == 0)
+// Recursively find the base pairs that form the maximum number of matchings
+void findBasePairs(Molecule* mol, int l, int r) {
+    if (mol->curMatches == mol->maxPairs || mol->maxMatchings[l][r] == 0)
         return;
 
-    if (isValidPairing(mol->sequence[l], mol->sequence[r]) && !isKink(l, r)) {
+    if (isValidBasePair(mol->sequence[l], mol->sequence[r]) && !isKink(l, r)) {
         if (mol->maxMatchings[l + 1][r - 1] + 1 == mol->maxMatchings[l][r]) {
             mol->matches[mol->curMatches].i = l;
             mol->matches[mol->curMatches].j = r;
             mol->curMatches++;
-            calculateMatchings(mol, l + 1, r - 1);
+            findBasePairs(mol, l + 1, r - 1);
             return;
         }
     }
 
     for (int t = l + 1; t <= r; t++) {
-        if (isValidPairing(mol->sequence[t], mol->sequence[r]) && !isKink(t, r)) {
+        if (isValidBasePair(mol->sequence[t], mol->sequence[r]) && !isKink(t, r)) {
             if (mol->maxMatchings[l][t - 1] + mol->maxMatchings[t + 1][r - 1] + 1 == mol->maxMatchings[l][r]) {
                 mol->matches[mol->curMatches].i = t;
                 mol->matches[mol->curMatches].j = r;
                 mol->curMatches++;
-                calculateMatchings(mol, l, t - 1);
-                calculateMatchings(mol, t + 1, r - 1);
+                findBasePairs(mol, l, t - 1);
+                findBasePairs(mol, t + 1, r - 1);
                 return;
             }
         }
     }
 
-    calculateMatchings(mol, l, r - 1);
+    findBasePairs(mol, l, r - 1);
 }
 
+// Set the RNA sequence for the molecule
 void setSequence(Molecule* mol, const char* seq) {
     mol->sequence = strdup(seq);
 }
 
+// Create a new molecule with the given RNA sequence
 Molecule createMolecule(const char* seq) {
     Molecule mol;
     setSequence(&mol, seq);
@@ -90,45 +96,52 @@ Molecule createMolecule(const char* seq) {
         mol.maxMatchings[i] = (int*)calloc(mol.len, sizeof(int));
 
     clock_t start = clock();
-    calculateMaxMatchings(&mol);
+    calculateMaxBasePairs(&mol);
     clock_t stop = clock();
-    mol.exec_time = (double)(stop - start) / CLOCKS_PER_SEC * 1000;
+    mol.execTime = (double)(stop - start) / CLOCKS_PER_SEC * 1000;
 
-    mol.matches = (Match*)malloc(mol.ans * sizeof(Match));
+    mol.matches = (Match*)malloc(mol.maxPairs * sizeof(Match));
     mol.curMatches = 0;
-    calculateMatchings(&mol, 0, mol.len - 1);
+    findBasePairs(&mol, 0, mol.len - 1);
 
     return mol;
 }
 
+// Get the length of the RNA sequence
 int getSequenceLength(Molecule* mol) {
     return mol->len;
 }
 
+// Get the RNA sequence
 char* getSequence(Molecule* mol) {
     return mol->sequence;
 }
 
-int getMaxMatchings(Molecule* mol) {
-    return mol->ans;
+// Get the maximum number of base pairs
+int getMaxBasePairs(Molecule* mol) {
+    return mol->maxPairs;
 }
 
-void printPairs(Molecule* mol) {
-    for (int i = 0; i < mol->ans; i++)
+// Print the base pairs in a readable format
+void printBasePairs(Molecule* mol) {
+    for (int i = 0; i < mol->maxPairs; i++)
         printf("%c---%c  (%d, %d)\n", mol->sequence[mol->matches[i].i], mol->sequence[mol->matches[i].j],
                mol->matches[i].i, mol->matches[i].j);
 }
 
-void printBases(Molecule* mol) {
-    for (int i = 0; i < mol->ans; i++)
+// Print the base pairs in a format suitable for writing to a file
+void printBasePairsToFile(Molecule* mol) {
+    for (int i = 0; i < mol->maxPairs; i++)
         printf("%d %d %c %c\n", mol->matches[i].i, mol->matches[i].j,
                mol->sequence[mol->matches[i].i], mol->sequence[mol->matches[i].j]);
 }
 
+// Get the execution time for calculating the maximum base pairs
 double getExecutionTime(Molecule* mol) {
-    return mol->exec_time;
+    return mol->execTime;
 }
 
+// Free the memory allocated for the molecule
 void freeMolecule(Molecule* mol) {
     free(mol->sequence);
     for (int i = 0; i < mol->len; i++)
@@ -146,16 +159,16 @@ int main() {
     Molecule rnaSequence = createMolecule(seq);
     printf("RNA sequence:                      %s\n", getSequence(&rnaSequence));
     printf("Length of the RNA sequence:        %d\n", getSequenceLength(&rnaSequence));
-    printf("Maximum number of base pairs:      %d\n", getMaxMatchings(&rnaSequence));
+    printf("Maximum number of base pairs:      %d\n", getMaxBasePairs(&rnaSequence));
     printf("Time taken to calculate max pairs: %.3fms\n", getExecutionTime(&rnaSequence));
     printf("The base pairs are:\n");
-    printPairs(&rnaSequence);
+    printBasePairs(&rnaSequence);
     printf("------------------------------------------------------------------------------------------------------------------------\n\n");
 
     FILE* fp = fopen("rna_secondary_structure.txt", "w");
     fprintf(fp, "%s\n", getSequence(&rnaSequence));
-    fprintf(fp, "%d\n", getMaxMatchings(&rnaSequence));
-    printBases(&rnaSequence);
+    fprintf(fp, "%d\n", getMaxBasePairs(&rnaSequence));
+    printBasePairsToFile(&rnaSequence);
     fclose(fp);
 
     freeMolecule(&rnaSequence);
